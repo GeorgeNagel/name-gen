489ae686b5f4f4051ccc0319bb8b2d536d6c6cd9
George Nagel <gsnagel@gmail.com>
2016-07-16 13:51:47 -0400

    Trying to make this distributable

---
+++ README.rst
@@ -0,0 +1,8 @@
+Name Generator
+=======================
+
+A helpful tool for generating random names.
+
+----
+
+To use `pip install` the project, and `import name_gen; name_gen.generate_name('male')
---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='NameGen',
+      version='0.1',
+      description='Python tool for random name generation',
+      author='George Nagel',
+      author_email='',
+      url='https://github.com/GeorgeNagel/name-gen.git',
+      packages=[],
+)
