07cc6badee4cf04214fb837a4c5f6b93267fe114
George Nagel <gsnagel@gmail.com>
2016-07-16 14:09:15 -0400

    Hoping to find missing packages

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from distutils.core import setup
+import setuptools
 
-setup(name='NameGen',
-      version='0.0.1',
-      description='Python tool for random name generation',
-      author='George Nagel',
-      author_email='',
-      url='https://github.com/GeorgeNagel/name-gen.git',
-      packages=['name_gen'],
+setup(
+    name='NameGen',
+    version='0.0.1',
+    description='Python tool for random name generation',
+    author='George Nagel',
+    author_email='',
+    url='https://github.com/GeorgeNagel/name-gen.git',
+    packages=setuptools.find_packages(exclude=['*.tests']),
 )
